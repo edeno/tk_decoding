40ac6e9d877c5d4ceb7757f0c9b029920155e176
Eric Denovellis <edenovellis@gmail.com>
2023-04-12 23:19:50 -0700

    Add directories

--- src/parameters.py
+++ src/parameters.py
@@ -1,2 +1,10 @@
+import os
+
 SAMPLING_FREQUENCY = 250  # samples per second
 CM_PER_PIXEL = 1 / 3.14  # cm / pixels
+
+
+# Data directories and definitions
+ROOT_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), os.path.pardir)
+PROCESSED_DATA_DIR = os.path.join(ROOT_DIR, "Processed-Data")
+RAW_DATA_DIR = os.path.join(ROOT_DIR, "Raw-Data")
